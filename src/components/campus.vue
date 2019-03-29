05b8b357b82f978c815cbc8474ebe7d97245da32
zhouyaoyao <624490555@qq.com>
2019-03-29 17:45:27 +0800

    fix:通知公告

---
+++ src/components/campus.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="comm_item float_right">
+        <div class="campus_tit">全部校区</div>
+        <div v-for="(item,index) in campusList" :key="index" class="campus_list">{{item.name}}</div>
+        <div class="no_data" v-if="!campusList || campusList.length == 0"></div>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            campusList:[
+                {
+                    name:'接口文档发货就开始对方sdfdshf啊可见度发挥的空间符合可是大家'
+                },
+                {
+                    name:'接口文档发货就开始对方'
+                },
+                {
+                    name:'接口文档发货就开始对方'
+                }
+            ]
+        }
+    }
+}
+</script>
+<style lang="less" scoped>
+    .campus_list{
+        padding:12px 20px 10px 20px;
+        color: #666;
+        cursor: pointer;
+    }
+    .campus_list:hover{
+        color: #aa2f33;
+    }
+    .campus_tit{
+        border-left: 4px solid #aa2f33;
+        padding-left: 16px;
+        font-size: 16px;
+        font-weight: bold;
+        margin-bottom: 15px;
+        line-height: 16px;
+    }
+</style>
+
